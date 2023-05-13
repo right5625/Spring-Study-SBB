dc1e419f836f71dc7f041371f8657455f615c6f8
Jinho <right5625@naver.com>
2023-05-14 02:16:07 +0900

    Commit

---
+++ sbb/src/main/java/com/mysite/sbb/Repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.mysite.sbb.Repository;
+
+import com.mysite.sbb.Entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
